5d3333db1f1d8e1ec8efe1902fc53d09799c938f
saloni1623 <salonigandhi771@gmail.com>
2024-03-20 10:00:16 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,108 +1,27 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cart from "./UseContext/CartPage";
-import Restuarent from "./UseContext/Restuarent";
-import Wishlist from "./UseContext/Wishlist";
+import Cart from "./UsingRedux/CartPage";
+import Restuarent from "./UsingRedux/Restuarent";
+import store,{persister} from "./UsingRedux/Redux/store";
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+import Wishlist from "./UsingRedux/Wishlist";
+import CheckOut from "./UsingRedux/CheckOut";
 
-export const Usercontext = React.createContext();
-
-function AppContext() {
-  const [cart, setcart] = useState([]);
-  const [wishes, setwishes] = useState([]);
-
-  const getCart = () => {
-    let ar = JSON.parse(localStorage.getItem("cart"));
-    if (ar) {
-      setcart(ar);
-    } else {
-      setcart([]);
-    }
-  };
-  const getwishes = () => {
-    let ar = JSON.parse(localStorage.getItem("wishes"));
-    if (ar) {
-      setwishes(ar);
-    } else {
-      setwishes([]);
-    }
-  };
-
-  useEffect(() => {
-    getCart();
-    getwishes();
-  }, []);
-
-  let handleCart = (data) => {
-    var flag = true;
-    let ar = JSON.parse(localStorage.getItem("cart"));
-    var i = 0;
-    var j = 0;
-    cart.map((el) => {
-      if (el.id == data.id) {
-        flag = false;
-        j = i;
-      }
-      i = i + 1;
-    });
-    if (ar) {
-      if (flag) {
-        ar.push(data);
-      } else {
-        ar.splice(j, 1);
-      }
-    } else {
-      ar = [data];
-    }
-    setcart(flag ? [...cart, data] : ar);
-
-    localStorage.setItem("cart", JSON.stringify(ar));
-  };
-
-  const handlewishes = (data) => {
-    var flag = true;
-    let ar = JSON.parse(localStorage.getItem("wishes"));
-    var i = 0;
-    var j = 0;
-    wishes.map((el) => {
-      if (el.id == data.id) {
-        flag = false;
-        j = i;
-      }
-      i = i + 1;
-    });
-    if (ar) {
-      if (flag) {
-        ar.push(data);
-      } else {
-        ar.splice(j, 1);
-      }
-    } else {
-      ar = [data];
-    }
-    setwishes(flag ? [...wishes, data] : ar);
-
-    localStorage.setItem("wishes", JSON.stringify(ar));
-  };
-  let cartItem = {
-    Cart: cart,
-    handleCart: handleCart,
-    wishes: wishes,
-    handlewishes: handlewishes,
-  };
-
+function App() {
   return (
-    <>
-      <Usercontext.Provider value={cartItem}>
-        <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persister}>
+    <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<Restuarent />} />
             <Route exact path="/cartpage" element={<Cart />} />
             <Route exact path="/wishlist" element={<Wishlist />} />
+            <Route exact path="/checkout" element={<CheckOut />} />
           </Routes>
-        </BrowserRouter>
-      </Usercontext.Provider>
-    </>
+    </BrowserRouter>
+    </PersistGate>
+    </Provider>
   );
 }
-export default AppContext;
+export default App
